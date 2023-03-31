8443cc2158b24d2b6b2cffea5958b2454d1ac8ef
Vivek <vivek@Viveks-MacBook-Pro.local>
2023-03-31 15:51:34 -0700

    changed icon back to original for android version

--- App.js
+++ App.js
@@ -123,7 +123,7 @@ export default function App() {
       <Header
         placement="center"
         centerComponent={{
-          text: "GPT - The Chat AI",
+          text: "ChatGPT Mobile",
           style: styles.toolbarTitle,
         }}
         containerStyle={{
@@ -155,7 +155,7 @@ export default function App() {
                 ]}
               >
                 {item.role === "assistant" && (
-                  <Text style={styles.assistantTitle}>GPT</Text>
+                  <Text style={styles.assistantTitle}>ChatGPT</Text>
                 )}
                 {item.role === "user" && (
                   <Text style={styles.userTitle}>You</Text>
