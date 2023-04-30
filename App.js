3ac4c59f92ed7a2a01d6dcddd6e2f01d54eceed7
Vivek <vivek@Viveks-MacBook-Pro.local>
2023-04-30 10:41:24 -0700

    changes to yoda's theme colours

--- App.js
+++ App.js
@@ -225,7 +225,7 @@ export default function App() {
   }, [messages]);
 
   return (
-    <KeyboardAvoidingView
+    <SafeAreaView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
       style={[
         styles.container,
@@ -426,7 +426,7 @@ export default function App() {
           <Ionicons name="send" size={24} color="#fff" />
         </TouchableOpacity>
       </View>
-    </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
