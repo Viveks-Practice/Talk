d49990a850bd79430cef9494638f69791e6dbddb
Vivek <vivek@Viveks-MacBook-Pro.local>
2023-05-23 17:43:46 -0700

    moved the login modal to another component

--- components/Header.js
+++ components/Header.js
@@ -13,6 +13,7 @@ import { Ionicons } from "@expo/vector-icons";
 import Constants from "expo-constants";
 import { Platform } from "react-native";
 import themes from "../themes.json";
+import LoginModal from "./LoginModal";
 
 const NeoHeader = ({ selectedOption, setModalVisible, theme }) => {
   const [loginModalVisible, setLoginModalVisible] = useState(false);
@@ -56,37 +57,10 @@ const NeoHeader = ({ selectedOption, setModalVisible, theme }) => {
         paddingTop: Platform.OS == "android" ? 35 : null,
       }}
     >
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={loginModalVisible}
-        onRequestClose={() => setLoginModalVisible(false)}
-      >
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <TextInput
-              style={styles.input}
-              placeholder="Username"
-              value={username}
-              onChangeText={setUsername}
-            />
-            <TextInput
-              style={styles.input}
-              placeholder="Password"
-              value={password}
-              onChangeText={setPassword}
-              secureTextEntry
-            />
-            <Button title="Log In" onPress={handleLogin} />
-            <Pressable
-              style={[styles.button, styles.buttonClose]}
-              onPress={() => setLoginModalVisible(!loginModalVisible)}
-            >
-              <Text style={styles.textStyle}>Hide Modal</Text>
-            </Pressable>
-          </View>
-        </View>
-      </Modal>
+      <LoginModal
+        isVisible={loginModalVisible}
+        onClose={() => setLoginModalVisible(false)}
+      />
     </Header>
   );
 };
@@ -111,57 +85,13 @@ const styles = StyleSheet.create({
       width: 0,
       height: 1,
     },
-    shadowOpacity: 0.22,
+    shadowOpacity: 0.9,
     shadowRadius: 2.22,
     elevation: 3,
   },
   loginButtonText: {
     color: "white",
     fontWeight: "bold",
-  },
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22,
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-  },
-  buttonOpen: {
-    backgroundColor: "#F194FF",
-  },
-  buttonClose: {
-    backgroundColor: "#2196F3",
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  input: {
-    height: 40,
-    width: 200,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
   },
 });
 
---
+++ components/LoginModal.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import {
+  Modal,
+  View,
+  TextInput,
+  Button,
+  Pressable,
+  Text,
+  StyleSheet,
+} from "react-native";
+
+const LoginModal = ({ isVisible, onClose }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    console.log(
+      `Logging in with username: ${username} and password: ${password}`
+    );
+    onClose();
+  };
+
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={isVisible}
+      onRequestClose={onClose}
+    >
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+          <TextInput
+            style={styles.input}
+            placeholder="Username"
+            value={username}
+            onChangeText={setUsername}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+          />
+          <Button title="Log In" onPress={handleLogin} />
+          <Pressable
+            style={[styles.button, styles.buttonClose]}
+            onPress={onClose}
+          >
+            <Text style={styles.textStyle}>Hide Modal</Text>
+          </Pressable>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  input: {
+    height: 40,
+    width: 200,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+
+export default LoginModal;
