f7f68e6f9ce605b78cb237c5f317764fe2376b1f
Vivek <vivek@Viveks-MacBook-Pro.local>
2023-07-10 20:09:08 -0700

    commented out the login button

--- components/Header.js
+++ components/Header.js
@@ -39,23 +39,23 @@ const NeoHeader = ({ selectedOption, setModalVisible, theme }) => {
           </Text>
         </Pressable>
       }
-      rightComponent={
-        <View style={styles.loginButtonContainer}>
-          <Pressable
-            style={[
-              styles.loginButton,
-              {
-                backgroundColor: themes[theme].colorSchemes.sixth,
-                shadowColor: themes[theme].colorSchemes.first,
-                borderColor: themes[theme].colorSchemes.fourth,
-              },
-            ]}
-            onPress={() => setLoginModalVisible(true)}
-          >
-            <Text style={styles.loginButtonText}>Log In</Text>
-          </Pressable>
-        </View>
-      }
+      // rightComponent={
+      //   <View style={styles.loginButtonContainer}>
+      //     <Pressable
+      //       style={[
+      //         styles.loginButton,
+      //         {
+      //           backgroundColor: themes[theme].colorSchemes.sixth,
+      //           shadowColor: themes[theme].colorSchemes.first,
+      //           borderColor: themes[theme].colorSchemes.fourth,
+      //         },
+      //       ]}
+      //       onPress={() => setLoginModalVisible(true)}
+      //     >
+      //       <Text style={styles.loginButtonText}>Log In</Text>
+      //     </Pressable>
+      //   </View>
+      // }
       containerStyle={{
         backgroundColor: themes[theme].colorSchemes.seventh,
         borderBottomColor: themes[theme].colorSchemes.eighth,
